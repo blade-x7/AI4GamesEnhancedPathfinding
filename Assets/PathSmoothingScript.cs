0e6221e36a6e4946e9d5152a72a461e741af27ef
blade-x7 <portia.flaherty@icloud.com>
2025-10-29 19:31:35 -0400

    editor buttons!

--- Assets/PathSmoothingScript.cs
+++ Assets/PathSmoothingScript.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
+using UnityEditor;
 
 public class PathSmoothingScript : MonoBehaviour
 {
     [SerializeField] GameObject dot;
     public List<Vector2> baseList;
-    public List<Vector2> interpolatedList;
+    List<Vector2> interpolatedList;
     [SerializeField] int samples;
+    List<GameObject> drawnPath;
 
     [SerializeField] Material blue;
 
@@ -46,7 +48,7 @@ public class PathSmoothingScript : MonoBehaviour
         return result;
     }
 
-    List<Vector2> Interpolate(List<Vector2> path, int samplesPerSegment)
+    public List<Vector2> Interpolate(List<Vector2> path, int samplesPerSegment)
     {
         if (path.Count < 2){
             return path;
@@ -72,21 +74,6 @@ public class PathSmoothingScript : MonoBehaviour
             return resultPath;
         }
 
-        /*
-        for (int segmentIndex = 0; segmentIndex < path.Count - 1; segmentIndex++)
-        {
-            //Vector2 currPoint = path[segmentIndex];
-            //Vector2 nextPoint = path[segmentIndex + 1];
-            //Vector2 pointAfterNext = (segmentIndex + 2 < path.Count) ? path[segmentIndex + 2] : path[segmentIndex + 1];
-
-            for (int sampleIndex = 1; sampleIndex <= samplesPerSegment; sampleIndex++)
-            {
-                float t = (float)sampleIndex / samplesPerSegment;
-                //Vector2 interpolatedPoint = QuadraticBezier(currPoint, nextPoint, pointAfterNext, t);
-                //resultPath.Add(interpolatedPoint);
-            }
-        }*/
-
         int p0, p1, p2, p3;
 
         for (float i = 0; i < (float)path.Count - 3.0f; i += 0.05f)
@@ -103,22 +90,52 @@ public class PathSmoothingScript : MonoBehaviour
         return resultPath;
     }
 
-    public void DrawPath()
+    public void DrawPath(List<Vector2> path)
     {
-        interpolatedList.Clear();
-        interpolatedList = Interpolate(baseList, samples);
+        for (int i = drawnPath.Count - 1; i >= 0; i--)
+        {
+            DestroyImmediate(drawnPath[i]);
+        }
+        drawnPath.Clear();
 
-        foreach (Vector2 i in baseList)
+        interpolatedList.Clear();
+        interpolatedList = Interpolate(path, samples);
+
+        foreach (Vector2 i in path)
         {
             Vector3 newPoint = new Vector3(i.x, 0.0f, i.y);
             GameObject newObject = GameObject.Instantiate(dot, newPoint, Quaternion.identity);
             newObject.gameObject.GetComponent<MeshRenderer>().material = blue;
+            drawnPath.Add(newObject);
         }
 
         foreach (Vector2 i in interpolatedList)
         {
             Vector3 newPoint = new Vector3(i.x, 0.0f, i.y);
-            GameObject.Instantiate(dot, newPoint, Quaternion.identity);
+            GameObject newObject = GameObject.Instantiate(dot, newPoint, Quaternion.identity);
+            drawnPath.Add(newObject);
+        }
+    }
+}
+
+
+[CustomEditor(typeof(PathSmoothingScript))]
+public class PathSmoothingEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var smoothing = (PathSmoothingScript)target;
+
+        EditorGUILayout.LabelField("Path Smoothing");
+
+        if (GUILayout.Button("Interpolate Path", GUILayout.Width(200)))
+        {
+            smoothing.Interpolate(smoothing.baseList, 7);
+        }
+
+        if (GUILayout.Button("Draw Path", GUILayout.Width(200)))
+        {
+            smoothing.DrawPath(smoothing.baseList);
         }
     }
 }
